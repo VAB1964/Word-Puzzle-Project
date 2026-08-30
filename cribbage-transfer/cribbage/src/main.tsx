6687956fd577e2bcd26855b6568ec79f853ded89
VAB1964 <vincebr@aol.com>
2026-08-30 12:53:27 -0700

    Remove migrated Cribbage application from Word Puzzle repository

--- cribbage-transfer/cribbage/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import AppEntry from "./components/AppEntry";
-import "./styles.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <AppEntry />,
-);
